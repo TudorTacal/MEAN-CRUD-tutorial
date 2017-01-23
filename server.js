ca645573ad6bc868d2f2115509c2235635464a13
TudorTacal <tudortacal@gmail.com>
2017-01-23 19:44:48 +0000

    Serves an index.html to the browser

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.listen(3000, function () {
 // That’s a good sign. It means we can now communicate to our express server through the browser.
 
 app.get('/', (req, res) => {
-  res.send("Hello world");
+  res.sendFile(__dirname + '/index.html');
+  // Note: __dirname is directory that contains the JavaScript source code.
+
 });
 // replaced the function with the arrow function
 // Note: request and response are usually written as req and res respectively.
