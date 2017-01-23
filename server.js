3c530bf80ac8d704db91abd7bff6b337a2720d9e
TudorTacal <tudortacal@gmail.com>
2017-01-23 19:32:18 +0000

    Added a get '/' route that shows 'Hello world'

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+/*jshint esversion: 6 */
 const express = require('express');
 const app = express();
 
 //  The first thing we want to do is to create a server
 //  where browsers can connect to. We can do so with the help of a
 //  listen method provided by Express:
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
+
+// That’s a good sign. It means we can now communicate to our express server through the browser.
+
+app.get('/', (req, res) => {
+  res.send("Hello world");
+});
+// replaced the function with the arrow function
+// Note: request and response are usually written as req and res respectively.
