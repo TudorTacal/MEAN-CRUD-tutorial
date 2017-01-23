7651dcf018dc1b58478870b179a1bbf5e075840a
TudorTacal <tudortacal@gmail.com>
2017-01-23 19:21:49 +0000

    Required express in server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+//  The first thing we want to do is to create a server
+//  where browsers can connect to. We can do so with the help of a
+//  listen method provided by Express:
